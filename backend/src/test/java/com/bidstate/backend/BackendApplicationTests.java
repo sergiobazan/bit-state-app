f2868c6a17f18a8e442a2577f5ba0a032a7450c2
Sergio Bazan <55011041+sergiobazan@users.noreply.github.com>
2025-02-18 23:24:59 -0500

    create backend

---
+++ backend/src/test/java/com/bidstate/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidstate.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
